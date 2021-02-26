c11e6c7ca06c170b0a94222b60850f98b293d4b1
unknown <kumar.skm03@gmail.com>
2021-02-26 11:33:40 +0530

    added method to choose X or O by user in TicTacToeGame.java

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,14 +1,37 @@
+import java.util.*;
 
 public class TicTacToeGame {
 
-	private static char board[] = new char[10];
+	static Scanner sc = new Scanner(System.in);
 
+	private static char board[] = new char[10];
+	private static char computer;
+	private static char player;
+
+	//UC1
 	private static void createBoard() {
 		for (int i = 1; i < board.length; i++)
 			board[i] = ' ';
 	}
 
-	public static void main(String args) {
+	//UC2
+	private static void choose() {
+		System.out.print("Choose X or O: ");
+		player = sc.next().toUpperCase().charAt(0);
+		if (player == 'X')
+			computer = 'O';
+		else if (player == 'O')
+			computer = 'X';
+		else {
+			System.out.println("Invalid input enter again");
+			choose();
+		}
+	}
+
+	public static void main(String args[]) {
 		createBoard();
+		choose();
+		System.out.println("Computer " + computer);
+		System.out.println("Player " + player);
 	}
 }
