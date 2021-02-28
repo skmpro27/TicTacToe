9ffed69328036627e099541eb0915e93e890e5a7
unknown <kumar.skm03@gmail.com>
2021-02-28 16:28:15 +0530

    added method to make desired location in TicTacToeGame.java

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class TicTacToeGame {
 
-	static Scanner sc = new Scanner(System.in);
+	private static Scanner sc = new Scanner(System.in);
 
 	private static char board[] = new char[10];
 	private static char computer;
@@ -16,14 +16,14 @@ public class TicTacToeGame {
 
 	//UC2
 	private static void choose() {
-		System.out.print("Choose X or O: ");
+		System.out.print("\nChoose your letter X or O: ");
 		player = sc.next().toUpperCase().charAt(0);
 		if (player == 'X')
 			computer = 'O';
 		else if (player == 'O')
 			computer = 'X';
 		else {
-			System.out.println("Invalid input enter again");
+			System.out.println("\nInvalid input enter again");
 			choose();
 		}
 	}
@@ -33,18 +33,28 @@ public class TicTacToeGame {
         	for (int i = 1; i < board.length; i++) {
                 	if (i%3 == 0 ) {
                         	System.out.println("  " + board[i]);
-                                System.out.println((i < board.length - 1) ? "-----+-----+------" : "");
+                                System.out.println((i < board.length - 1) ? "-----+-----+-----" : "");
                         }
                         else
                                 System.out.print("  " + board[i] + "  |");
                 }
         }
 
+        //UC4
+        public static void playerTurn() {
+                System.out.print("Select a position to play(1-9): ");
+                int position = sc.nextInt();
+                if (board[position] != ' ') {
+                        System.out.println("Position occupied select another");
+                }
+        }
+
 	//main
 	public static void main(String args[]) {
+
+		System.out.println("Welcome to Tic Tac Toe Game");
 		createBoard();
 		choose();
-		System.out.println("You letter is " + player);
 		System.out.println("Computer letter is  " + computer);
 		printBoard();
 	}
